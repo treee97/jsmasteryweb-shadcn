ee6b2670f6059b52a1718e0e5e4bb13015a4d682
treee97 <luduve97@gmail.com>
2023-09-30 01:23:33 +0200

    added sanity - preparing filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,5 +1,21 @@
+import Filters from "@/components/Filters";
+import SearchForm from "@/components/SearchForm";
+
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col  ">
+      <section className="nav-padding w-full ">
+        {/* nav-padding: pt-[98px] that explains why the box is not in the middle*/}
+        <div className="flex-center relative min-h-[274px] w-full rounded-xl bg-banner ">
+          <h1 className="sm:heading1 heading2 mb-6 text-center text-white ">
+            Javascript Mastery Resources
+          </h1>
+        </div>
+        <SearchForm />
+      </section>
+      <Filters />
+    </main>
+  );
 };
 
 export default Page;
