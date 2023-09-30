13ffe70a551d2963eee73002305c7abef87b1127
treee97 <luduve97@gmail.com>
2023-09-30 11:06:02 +0200

    schemas added & doc to sanity-studio, pre-fetching

---
+++ sanity/schemas/resource.schema.ts
@@ -0,0 +1,44 @@
+import { Ruler } from "lucide-react";
+
+const schema = {
+  name: "resource",
+  title: "Resource",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+      require,
+      validation: (Ruler: any) => Ruler.required(),
+    },
+    { name: "slug", title: "Slug", type: "slug", options: { source: "title" } },
+    {
+      name: "downloadLink",
+      title: "Download Link",
+      type: "url",
+      validation: (Rule: any) => Rule.required(),
+    },
+    { name: "views", title: "Views", type: "number", initialValue: 0 },
+    {
+      name: "poster",
+      title: "Poster",
+      type: "image",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: "category",
+      title: "Category",
+      type: "string",
+      validation: (Rule: any) => Rule.required(),
+      options: {
+        list: ["fronted", "backend", "next 13", "fullstack", "other"],
+      },
+    },
+  ],
+};
+
+export default schema;
