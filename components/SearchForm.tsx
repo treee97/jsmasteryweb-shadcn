1757b927a72e4ba7f8e453988213059135d7b96a
treee97 <luduve97@gmail.com>
2023-10-05 20:36:00 +0200

    !! DEBOUNCING

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,10 +1,19 @@
 "use client";
 import Image from "next/image";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+//DEBOUNCING
 
 const SearchForm = () => {
-  const [Search, setSearch] = useState("");
+  const [search, setSearch] = useState("");
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      console.log(search);
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
+
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
       <label className="flex-center relative w-full max-w-3xl">
@@ -19,6 +28,7 @@ const SearchForm = () => {
           className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
           type="text"
           placeholder="Search"
+          onChange={(e) => setSearch(e.target.value)}
         />
       </label>
     </form>
