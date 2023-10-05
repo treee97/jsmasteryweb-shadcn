9062f606889d64706eeae7c64b90f8b8d1602636
treee97 <luduve97@gmail.com>
2023-10-06 00:24:42 +0200

    Skeleton shadcn-ui added

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,5 +1,21 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 const loading = () => {
-  return <div>loading</div>;
+  return (
+    <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
+      <section className="nav-padding w-full">
+        <Skeleton className="w-full h-[274px] rounded-lg bg-white-800" />
+      </section>
+      <section className="mt-6 flex w-full flex-col sm:mt-20">
+        <Skeleton className="h-10 w-56 bg-white-800" />
+        <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-white-800" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-white-800" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-white-800" />
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default loading;
